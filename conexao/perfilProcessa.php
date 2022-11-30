cd960ff294fcebf724e05249ff8bfa6ee4f52357
arthurml17 <117524657+arthurml17@users.noreply.github.com>
2022-11-30 02:08:01 -0300

    segundo commit

--- conexao/perfilProcessa.php
+++ conexao/perfilProcessa.php
@@ -10,7 +10,7 @@ $emailusu = $_POST['emailusu'];
 $senhausu = $_POST['senhausu'];
 
 $sql = "UPDATE perfil SET nomeusu='$nomeusu', emailusu = '$emailusu', senhausu = '$senhausu' WHERE id = '$id'";
-var_dump($sql);
+
 $resultado_usu = mysqli_query($conn ,$sql);
 
 header("Location: https://localhost/projetoPHP/index.php");
