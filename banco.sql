137eae0c5bf2f3f4092f119be869a555ddcc6a60
arthurml17 <117524657+arthurml17@users.noreply.github.com>
2022-12-07 08:48:50 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -11,9 +11,26 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Copiando dados para a tabela projeto_php.newsletter: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `newsletter` DISABLE KEYS */;
-/*!40000 ALTER TABLE `newsletter` ENABLE KEYS */;
+-- Copiando estrutura do banco de dados para projeto_php
+CREATE DATABASE IF NOT EXISTS `projeto_php` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+USE `projeto_php`;
+
+-- Copiando estrutura para tabela projeto_php.newsletter
+CREATE TABLE IF NOT EXISTS `newsletter` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela projeto_php.perfil
+CREATE TABLE IF NOT EXISTS `perfil` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nomeusu` varchar(100) COLLATE utf8_unicode_ci DEFAULT '0',
+  `emailusu` varchar(100) COLLATE utf8_unicode_ci DEFAULT '0',
+  `senhausu` varchar(100) COLLATE utf8_unicode_ci DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- Copiando dados para a tabela projeto_php.perfil: ~2 rows (aproximadamente)
 /*!40000 ALTER TABLE `perfil` DISABLE KEYS */;
